2148bf3380fcbabab13c0a4c834c0fcfbd3a4660
DLuz1 <diogolinoluz@hollandandbarrett.com>
2023-03-13 12:50:45 +0000

    refactor(_app.tsx): replaced GameContext.Provider with new AppWrapper Component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import GameContext from "@/context/GameContext";
+import { AppWrapper } from "@/context/GameContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <GameContext.Provider value={[]}>
+    <AppWrapper>
       <Component {...pageProps} />
-    </GameContext.Provider>
+    </AppWrapper>
   );
 }
