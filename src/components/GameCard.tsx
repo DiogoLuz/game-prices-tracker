18a0863698e886323a07abcb53097e68e75e8c7a
DLuz1 <diogolinoluz@hollandandbarrett.com>
2023-03-14 07:33:08 +0000

    feat(GameCard): added extra styles and created Next.js Link with button element inside to take you to said page

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import { GameContext } from "@/context/GameContext";
 import { GameContextType } from "@/context/GameContext";
 import Image from "next/image";
 import { Game } from "@/context/GameContext";
+import Link from "next/link";
 
 type Props = { game: Game };
 
@@ -12,7 +13,7 @@ function GameCard({ game }: Props) {
   console.log(game.thumb);
   return (
     // Use flex and flex-col utilities to create a card container
-    <div className="flex max-w-sm flex-col overflow-hidden rounded bg-black shadow-lg">
+    <div className="flex max-w-sm flex-col overflow-hidden rounded border-2 border-white bg-black shadow-lg">
       {/* // Use relative and h-48 utilities to create an image section */}
       <div className="relative h-48" id="image-section">
         {/* // Use absolute and inset-0 utilities to position the image */}
@@ -27,10 +28,14 @@ function GameCard({ game }: Props) {
         )}
       </div>
       {/* // Use p-4 utility to add padding to the content section */}
-      <div className="p-4">
+      <div className="flex flex-col items-center gap-2 p-4">
         {/* // Use text-white utility to change the text color */}
         <h3 className="text-md text-white">{game.external}</h3>
-        <p className="text-sm text-white">Cheapest Price: {game.cheapest}</p>
+        <Link href={`/games/${game.internalName.toLowerCase()}`}>
+          <button className="rounded border-2 border-white bg-sky-600 p-1 text-white hover:bg-sky-900">
+            See the deals
+          </button>
+        </Link>
       </div>
     </div>
   );
