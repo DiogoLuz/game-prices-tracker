6d9f20fa4cd6165a902f2b9e218a051912169044
DLuz1 <diogolinoluz@hollandandbarrett.com>
2023-03-14 07:50:52 +0000

    feat(game): built out game page a little bit more

--- src/pages/games/[game].tsx
+++ src/pages/games/[game].tsx
@@ -1,12 +1,26 @@
+import Navbar from "@/components/Navbar";
+import { GameContext } from "@/context/GameContext";
 import { NextPage, GetServerSideProps } from "next";
 import { useRouter } from "next/router";
+import { useContext } from "react";
 
 interface Props {}
 
 const Game: NextPage<Props> = ({}) => {
   const router = useRouter();
   const { game } = router.query;
-  return <div>{game}</div>;
+  const { gameList, setGameList } = useContext(GameContext);
+  console.log(gameList);
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <header>
+          <h1>{gameList[0].external}</h1>
+        </header>
+      </main>
+    </div>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async ({
